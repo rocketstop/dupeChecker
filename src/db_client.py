cfe6ff6b359579d770615c2d0b832219fb2be7be
Mike Brown <mike@mostlysafe.com>
2023-01-29 19:20:11 -0600

    adding exception handling to mongoclient

--- src/db_client.py
+++ src/db_client.py
@@ -1,7 +1,10 @@
-from typing import Any, List
+from typing import Any, List, Optional, Mapping
 
 import pymongo
 import time
+import logging
+
+from pymongo.cursor import Cursor
 
 
 class MongoHashClient:
@@ -15,37 +18,62 @@ class MongoHashClient:
         self._db = self._db_client[hash_database]
         self._collection = self._db["file_hashes_"+current_time]
 
-    def add_document(self, document: dict) -> Any:
+    def add_document(self, document: dict) -> Optional[str]:
         if self._collection is not None:
-            r = self._collection.insert_one(document)
-            return r.inserted_id
+            try:
+                r = self._collection.insert_one(document)
+                return r.inserted_id
+            except:
+                logging.error("Exception in add_document")
+            finally:
+                return None
 
-    def add_documents(self, documents: List[dict]) -> List[Any]:
+    def add_documents(self, documents: List[dict]) -> Optional[List]:
         if self._collection is not None:
-            r = self._collection.insert_many(documents)
-            return r.inserted_ids
+            try:
+                r = self._collection.insert_many(documents)
+                return r.inserted_ids
+            except:
+                logging.error("Exception in add_documents")
+            finally:
+                return None
 
-    def find_by_id(self, id: str):
+    def find_by_id(self, id: str) -> Optional[str]:
         if self._collection is not None:
-            r = self._collection.find_one({"_id": id})
-            return r
+            try:
+                r = self._collection.find_one({"_id": id})
+                return r
+            except:
+                logging.error("Exception in find_one")
+            finally:
+                return None
 
-    def find_by_hash(self, hash: str):
+    def find_by_hash(self, hash: str) -> Optional[Cursor[Mapping[str, Any]]]:
         if self._collection is not None:
-            docs = self._collection.find({"file_hash": hash})
-            return docs
+            try:
+                docs = self._collection.find({"file_hash": hash})
+                return docs
+            except:
+                logging.error("Exception in find_by_hash")
+            finally:
+                return None
 
     def find_duplicate_hashes(self):
         if self._collection is not None:
-            duplicates = self._collection.aggregate(
-                [
-                    {"$match": {"file_hash": {"$ne": None}}},
-                    {"$group": {"_id": "$file_hash", "files": {"$addToSet": "$filename"}, "count": {"$sum": 1}}},
-                    {"$match": {"count": {"$gt": 1}}},
-                    {"$project": {"file_hash": "$_id", "_id": 0, "count": 1, "files": 1}}
-                ]
-            )
-            return duplicates
+            try:
+                duplicates = self._collection.aggregate(
+                    [
+                        {"$match": {"file_hash": {"$ne": None}}},
+                        {"$group": {"_id": "$file_hash", "files": {"$addToSet": "$filename"}, "count": {"$sum": 1}}},
+                        {"$match": {"count": {"$gt": 1}}},
+                        {"$project": {"file_hash": "$_id", "_id": 0, "count": 1, "files": 1}}
+                    ]
+                )
+                return duplicates
+            except:
+                logging.error("Exception in find_duplicate_hashes")
+            finally:
+                return None
 
 
 def test():
